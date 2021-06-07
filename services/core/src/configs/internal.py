2e10af6239bda83658db08fee70332a444e13456
Andrey Grishin <andrey.grishin@startted.com>
2021-06-07 13:10:20 +0300

    Настройка CI

--- services/core/src/configs/internal.py
+++ services/core/src/configs/internal.py
@@ -1,3 +1,5 @@
-FILES_LINK = 'some-link'
-SECRET_KEY = 'qwerty123'
-ENCRYPT_ALGORITHM = 'HS256'
+from src.extra.utils.config import ConfigUtils
+
+FILES_LINK = ConfigUtils.env('PODGOTOVKA_FILES_LINK', str)
+SECRET_KEY = ConfigUtils.env('PODGOTOVKA_SECRET_KEY', str)
+ENCRYPT_ALGORITHM = ConfigUtils.env('PODGOTOVKA_ENCRYPT_ALGORITHM', str)
