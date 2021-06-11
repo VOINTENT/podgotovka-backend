f1ceb437d0de6e4ab410e46b943ce9c9088297f4
Andrey Grishin <andrey.grishin@startted.com>
2021-06-11 18:23:31 +0300

    Обновлена моделька для списка уроков для препода

--- services/core/src/internal/biz/entities/response/subject/simple.py
+++ services/core/src/internal/biz/entities/response/subject/simple.py
@@ -4,5 +4,6 @@ from src.internal.biz.entities.response.base import BaseResponseModel
 
 
 class SubjectSimpleResponse(BaseResponseModel):
-    id: int = Field(None, example=1)
     name: str = Field(None, example='Название')
+    course_id: int = Field(..., example=1)
+    subject_id: int = Field(..., example=2)
